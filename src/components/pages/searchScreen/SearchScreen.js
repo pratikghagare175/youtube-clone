af9a88ea201c3b6fa7889495a8c52652816ecc15
pratikghagare175 <pratikghagare1797@gmail.com>
2021-07-06 23:33:14 +0530

    Search screen started

--- src/components/pages/searchScreen/SearchScreen.js
+++ src/components/pages/searchScreen/SearchScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { fetchVideoBySearch } from "../../../redux/slices/searchScreenSlice";
 
 const SearchScreen = () => {
   const { query } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchVideoBySearch({ query }));
+  }, [dispatch, query]);
   return (
     <div>
       <h1>{query}</h1>
