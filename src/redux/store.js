aaea144ad9a47488c43289503cd54f859ff1d4c9
pratikghagare175 <pratikghagare1797@gmail.com>
2021-07-06 23:33:28 +0530

    added new reducer for searchScreen

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import homeVideosReducer from "./slices/homeVideosSlice";
 import watchScreenReducer from "./slices/watchScreenSlice";
+import searchScreenReducer from "./slices/searchScreenSlice";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     homeVideos: homeVideosReducer,
     watchScreen: watchScreenReducer,
+    searchScreen: searchScreenReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
