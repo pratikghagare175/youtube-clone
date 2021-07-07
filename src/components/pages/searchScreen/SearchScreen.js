df9257feb384d572f8383754d67a3e7943da1268
pratikghagare175 <pratikghagare1797@gmail.com>
2021-07-07 09:21:45 +0530

    Implementing UI for search Screen

--- src/components/pages/searchScreen/SearchScreen.js
+++ src/components/pages/searchScreen/SearchScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchVideoBySearch } from "../../../redux/slices/searchScreenSlice";
+import VideoHorizontal from "../../videoHorizontal/VideoHorizontal";
 
 const SearchScreen = () => {
   const { query } = useParams();
@@ -11,10 +12,16 @@ const SearchScreen = () => {
   useEffect(() => {
     dispatch(fetchVideoBySearch({ query }));
   }, [dispatch, query]);
+
+  const { videos, loading } = useSelector((state) => state.searchScreen);
   return (
-    <div>
-      <h1>{query}</h1>
-    </div>
+    <>
+      {!loading ? (
+        videos?.map((video) => <VideoHorizontal video={video} searchScreen />)
+      ) : (
+        <h2>Loading ...</h2>
+      )}
+    </>
   );
 };
 
