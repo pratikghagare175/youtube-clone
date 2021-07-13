d5adf9aec86ebdedcd99501a082f73199abde233
pratikghagare175 <pratikghagare1797@gmail.com>
2021-07-13 23:19:42 +0530

    removed unused import

--- src/components/videoSkeleton/HorizontalSkeleton.js
+++ src/components/videoSkeleton/HorizontalSkeleton.js
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
 import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
 import Skeleton from "@material-ui/lab/Skeleton";
 
 const useStyles = makeStyles((theme) => ({
