ad5f8cb659e76c26a6678b76fd907b140b624534
pratikghagare175 <pratikghagare1797@gmail.com>
2021-07-13 23:16:29 +0530

    search skeleton added on loading the component

--- src/components/pages/searchScreen/SearchScreen.js
+++ src/components/pages/searchScreen/SearchScreen.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchVideoBySearch } from "../../../redux/slices/searchScreenSlice";
 import VideoHorizontal from "../../videoHorizontal/VideoHorizontal";
+import SearchSkeleton from "../../videoSkeleton/SearchSkeleton";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginTop: "-2.5rem",
+    [theme.breakpoints.down("xs")]: {
+      marginTop: "0",
+    },
+  },
+}));
 
 const SearchScreen = () => {
   const { query } = useParams();
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(fetchVideoBySearch({ query }));
@@ -15,13 +28,11 @@ const SearchScreen = () => {
 
   const { videos, loading } = useSelector((state) => state.searchScreen);
   return (
-    <>
-      {!loading ? (
-        videos?.map((video) => <VideoHorizontal video={video} searchScreen />)
-      ) : (
-        <h2>Loading ...</h2>
-      )}
-    </>
+    <div className={classes.root}>
+      {!loading
+        ? videos?.map((video) => <VideoHorizontal video={video} searchScreen />)
+        : videos?.map((val, i) => <SearchSkeleton />)}
+    </div>
   );
 };
 
