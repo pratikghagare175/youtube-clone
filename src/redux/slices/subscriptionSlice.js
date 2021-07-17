d64c01f9981436f047b90df4f2060de5ba1bdd35
pratikghagare175 <pratikghagare1797@gmail.com>
2021-07-17 23:38:04 +0530

    New Slice Created for subscription screen

---
+++ src/redux/slices/subscriptionSlice.js
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../Axios";
+
+export const fetchSubscribedChannels = createAsyncThunk(
+  "yt_subscriptionscreen/fetchSubscribedChannels",
+  async (payload, { rejectWithValue, getState }) => {
+    try {
+      const { data } = await axios("/subscriptions", {
+        params: {
+          part: "snippet,contentDetails",
+          mine: true,
+        },
+        headers: {
+          Authorization: `Bearer ${getState().auth.accessToken}`,
+        },
+      });
+
+      return {
+        channels:data.items,
+      };
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+const subscriptionSlice = createSlice({
+  name: "yt_subscriptionscreen",
+  initialState: {
+    loading: false,
+    subscriptions: [],
+  },
+  extraReducers: {
+    //? Thunk To get Subscribed Channels
+    [fetchSubscribedChannels.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [fetchSubscribedChannels.fulfilled]: (state, action) => {
+      const { channels } = action.payload;
+      state.loading = false;
+      state.subscriptions = channels
+    },
+    [fetchSubscribedChannels.rejected]: (state, action) => {
+      state.loading = false;
+    },
+  },
+});
+
+export default subscriptionSlice.reducer;
